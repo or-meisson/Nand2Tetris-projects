e2f3303f8cc521233e4d95ac8a2ef1e45c82c377
or-meisson <orelim1@gmail.com>
2023-11-16 20:32:47 +0200

    Update Swap.asm

--- 04/swap/Swap.asm
+++ 04/swap/Swap.asm
@@ -15,3 +15,120 @@
 // - Changing R14, R15 is not allowed.
 
 // Put your code here.
+
+@R14 // get the adress of the first element
+D=M
+
+//set both min and max to point the same value as the first element
+@R1
+M=D
+
+@R2
+M=D
+
+@R3 //set a counter to check if we finished the array
+M=0
+
+(LOOP)
+    @R3 //check if we finished the array
+    D=M
+    @R15
+    D=D-M
+    @SWAP //if we finished the array swap the min and max
+    D;JEQ
+
+    //else (didn't finish the array)
+
+    @R3 //we update the counter
+    M=M+1
+    D=M  //and store the index
+
+    @R14 //we check the adress of the first element
+    A=M
+
+    @R4 //we save in R4 the updated adress of the next element
+    M=D+A
+
+    @R4 //we dereference the current element
+    A=M
+    D=M
+    @R1
+    D=D-M //we check if the current element is less than the min
+    @NEW_MIN
+    D;JGT //if so, set a new min
+
+    @R4 //we dereference the current element
+    A=M
+    D=M
+    @R2
+    D=D-M //we check if the current element is greater than the max
+    @NEW_MAX
+    D;JGT //if so, set a new max
+
+    @LOOP
+    0;JMP
+
+
+(NEW_MIN)
+    @R4
+    D=M //a pointer to the element we want to switch with
+
+    @R1 //switch to new min
+    M=D
+
+    @LOOP
+    0;JMP
+
+(NEW_MAX)
+    @R4
+    D=M
+
+    @R2 //switch to new max
+    M=D
+
+    @LOOP
+    0;JMP
+
+(SWAP)
+    @R1
+    D=M
+    
+    @temp
+    M=D
+
+    @R2
+    D=M
+
+    @R1
+    M=D
+
+    @temp
+    D=M
+
+    @R2
+    M=D
+
+    @END
+    0;JMP
+
+
+(END)
+    @END
+    0;JMP
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
