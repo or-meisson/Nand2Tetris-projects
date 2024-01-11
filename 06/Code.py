f6d9ad975e6972b81f8b9ecd05ecb096d69eb45f
or-meisson <orelim1@gmail.com>
2024-01-11 19:03:24 +0200

    project 6
    first work on project 6

--- 06/Code.py
+++ 06/Code.py
@@ -16,11 +16,21 @@ class Code:
         Args:
             mnemonic (str): a dest mnemonic string.
 
-        Returns:
+        Returns:0
             str: 3-bit long binary code of the given mnemonic.
         """
-        # Your code goes here!
-        pass
+        dest_table = {
+            "": "000",
+            "M": "001",
+            "D": "010",
+            "DM": "011",
+            "MD": "011",
+            "A": "100",
+            "AM": "101",
+            "AD": "110",
+            "ADM": "111",
+        }
+        return dest_table[mnemonic]
 
     @staticmethod
     def comp(mnemonic: str) -> str:
@@ -31,8 +41,43 @@ class Code:
         Returns:
             str: the binary code of the given mnemonic.
         """
-        # Your code goes here!
-        pass
+        comp_table = {
+            "0": "0101010",
+            "1": "0111111",
+            "-1": "0111010",
+            "D": "0001100",
+            "A": "0110000",
+            "!D": "0001101",
+            "!A": "0110001",
+            "-D": "0001111",
+            "-A": "0110011",
+            "D+1": "0011111",
+            "A+1": "0110111",
+            "D-1": "0001110",
+            "A-1": "0110010",
+            "D+A": "0000010",
+            "D-A": "0010011",
+            "A-D": "0000111",
+            "D&A": "0000000",
+            "D|A": "0010101",
+            "M": "1110000",
+            "!M": "1110001",
+            "-M": "1110011",
+            "M+1": "1110111",
+            "M-1": "1110010",
+            "D+M": "1000010",
+            "D-M": "1010011",
+            "M-D": "1000111",
+            "D&M": "1000000",
+            "D|M": "1010101",
+            "D>>": "0010000",
+            "A>>": "0000000",
+            "M>>": "1000000",
+            "D<<": "0110000",
+            "A<<": "0100000",
+            "M<<": "1100000",
+        }
+        return comp_table[mnemonic]
 
     @staticmethod
     def jump(mnemonic: str) -> str:
@@ -43,5 +88,14 @@ class Code:
         Returns:
             str: 3-bit long binary code of the given mnemonic.
         """
-        # Your code goes here!
-        pass
+        jump_table = {
+            "": "000",
+            "JGT": "001",
+            "JEQ": "010",
+            "JGE": "011",
+            "JLT": "100",
+            "JNE": "101",
+            "JLE": "110",
+            "JMP": "111",
+        }
+        return jump_table[mnemonic]
--- 06/Parser.py
+++ 06/Parser.py
@@ -23,8 +23,12 @@ class Parser:
         """
         # Your code goes here!
         # A good place to start is to read all the lines of the input:
-        # input_lines = input_file.read().splitlines()
-        pass
+        self.input_lines = input_file.read().splitlines()
+        self.current_command_idx = 0
+        self.curr_command = None
+        self.dest = None
+        self.comp = None
+        self.jump = None
 
     def has_more_commands(self) -> bool:
         """Are there more commands in the input?
@@ -33,16 +37,19 @@ class Parser:
             bool: True if there are more commands, False otherwise.
         """
         # Your code goes here!
-        pass
+        return self.current_command_idx < len(self.input_lines)
 
     def advance(self) -> None:
         """Reads the next command from the input and makes it the current command.
         Should be called only if has_more_commands() is true.
         """
         # Your code goes here!
-        pass
+        command_with_comments = self.input_lines[self.current_command_idx]
+        self.curr_command = command_with_comments.split('//', 1)[0]. \
+            replace(" ", "").strip().strip()  # remove whitespaces and comments
+        self.current_command_idx += 1
 
-    def command_type(self) -> str:
+    def command_type(self) -> str:  # with no symbolic references
         """
         Returns:
             str: the type of the current command:
@@ -51,7 +58,12 @@ class Parser:
             "L_COMMAND" (actually, pseudo-command) for (Xxx) where Xxx is a symbol
         """
         # Your code goes here!
-        pass
+        if self.curr_command[0] == "@":
+            return "A_COMMAND"
+        elif is_c_command(self.curr_command)[0]:
+            return "C_COMMAND"
+        else:
+            return "L_COMMAND"  # dont know if this is correct
 
     def symbol(self) -> str:
         """
@@ -60,8 +72,10 @@ class Parser:
             (Xxx). Should be called only when command_type() is "A_COMMAND" or 
             "L_COMMAND".
         """
-        # Your code goes here!
-        pass
+        if self.curr_command[0] == "@":
+            return self.curr_command[1:]
+        else:
+            return self.curr_command
 
     def dest(self) -> str:
         """
@@ -69,8 +83,8 @@ class Parser:
             str: the dest mnemonic in the current C-command. Should be called 
             only when commandType() is "C_COMMAND".
         """
-        # Your code goes here!
-        pass
+
+        return self.dest
 
     def comp(self) -> str:
         """
@@ -78,8 +92,7 @@ class Parser:
             str: the comp mnemonic in the current C-command. Should be called 
             only when commandType() is "C_COMMAND".
         """
-        # Your code goes here!
-        pass
+        return self.comp
 
     def jump(self) -> str:
         """
@@ -87,5 +100,28 @@ class Parser:
             str: the jump mnemonic in the current C-command. Should be called 
             only when commandType() is "C_COMMAND".
         """
-        # Your code goes here!
-        pass
+        return self.jump
+
+
+    def is_c_command(self, string) -> bool:
+        parts = string.split('=')
+        # Check if there is exactly one '=' to separate dest from comp
+        if len(parts) == 2:
+            self.dest, comp_and_jump = parts
+            # Further split comp_and_jump based on ';'
+            comp_and_jump_parts = comp_and_jump.split(';')
+
+            # Check if there is at most one ';'
+            if len(comp_and_jump_parts) <= 2:
+                self.comp = comp_and_jump_parts[0]
+                # Check if there is a jump part (optional)
+                self.jump = comp_and_jump_parts[1] if len(
+                    comp_and_jump_parts) == 2 else None
+
+                # Check if dest, comp, and jump (if present) are valid
+                if self.dest and self.comp:
+                    # Here, you can perform additional validation if needed
+                    return True
+
+        # If the format is not matched, return False
+        return False
